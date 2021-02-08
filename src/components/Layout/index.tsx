db11833c5b8c2faf06dd48fa1569b9027d0048cc
leideFerreira <lmsf@icomp.ufam.edu.br>
2021-02-07 21:41:43 -0400

    Primeiro commit, styles ok

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container} from './styles';
+import MainHeader from '../mainHeader';
+import Aside from '../aside';
+import Content from '../content';
+
+const Layout: React.FC = () =>{
+    return (
+        <Container>
+           <MainHeader />
+           <Aside />
+           <Content />
+        </Container>
+    );
+}
+
+export default Layout;
---
+++ src/pages/Avaliacao/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Para mostrar a avaliacao do usuario de acordo com a area de dificuldade
+const Avaliacao: React.FC = () =>{
+    return (
+        <h1>Avaliacao</h1>
+    );
+}
+
+export default Avaliacao;
